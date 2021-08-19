84b1136334409ac4b2ea2e058ba9cc6bc5dd1791
teslaboy1 <baidyashiraj2@gmail.com>
2021-08-20 00:37:01 +0545

    added a comment

--- SalesTransaction.WebApi/Controllers/WeatherForecastController.cs
+++ SalesTransaction.WebApi/Controllers/WeatherForecastController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 using Microsoft.Extensions.Configuration;
-
+//nkv
 namespace SalesTransaction.WebApi.Controllers
 {
     [ApiController]
